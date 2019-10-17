7321a6ef2c9b51a61e92252e6ad3864220112dd8
Sarim bin Faisal <sarimbf.177@gmail.com>
2019-10-17 14:23:40 +0500

    media query added and removed JS

--- formToEmail.php
+++ formToEmail.php
@@ -1,7 +1,6 @@
 <?php
 if(!isset($_POST['submit']))
 {
-	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
 }
 $name = $_POST['name'];
@@ -9,25 +8,25 @@ $visitor_email = $_POST['email'];
 $phone = $_POST['phone'];
 $course = $_POST['course'];
 
-//Validate first
-// if(empty($name)||empty($visitor_email)||empty($phone)||empty($course)) 
-// {
-//     echo "Fill the empty fields!";
-//     exit;
-// }
+// Validate first
+if(empty($name)||empty($visitor_email)||empty($phone)||empty($course)) 
+{
+    echo "Fill the empty fields!";
+    exit;
+}
 
-// if(IsInjected($visitor_email))
-// {
-//     echo "Bad email value!";
-//     exit;
-// }
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
 
-$email_from = "sarimbf.177@hotmail.com";//<== update the email address
+$email_from = "youremail@domain.com";//<== update the email address
 $email_subject = "New Inquiry";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
     
-$to = "sarimbf.177@hotmail.com";//<== update the email address
+$to = "youremail@domain.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
@@ -37,26 +36,26 @@ header('Location: thank-you.html');
 
 
 // Function to validate against any email injection attempts
-// function IsInjected($str)
-// {
-//   $injections = array('(\n+)',
-//               '(\r+)',
-//               '(\t+)',
-//               '(%0A+)',
-//               '(%0D+)',
-//               '(%08+)',
-//               '(%09+)'
-//               );
-//   $inject = join('|', $injections);
-//   $inject = "/$inject/i";
-//   if(preg_match($inject,$str))
-//     {
-//     return true;
-//   }
-//   else
-//     {
-//     return false;
-//   }
-// }
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
    
 ?> 
